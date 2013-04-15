5725f75b9985227e1c8412c22e0128a5f1bdca25
Justas Palumickas <justas@elish.lt>
2013-04-15 11:25:18 +0300

    Add RSpec.
    * include LICENSE file.

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+gem 'rake'
+
+group :test do
+  gem 'rspec', '>= 2.13'
+  gem 'simplecov', :require => false
+end
+
+gemspec
---
+++ spec/spec_helper.rb
@@ -0,0 +1,23 @@
+require 'simplecov'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter
+]
+SimpleCov.start
+
+require 'csstats'
+require 'rspec'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
+
+def fixture_path
+  File.expand_path("../fixtures", __FILE__)
+end
+
+def fixture(file)
+  File.new(fixture_path + '/' + file)
+end

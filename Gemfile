7ff713587cc5c47a699b0b73912277d21b4766a6
Justas Palumickas <justas@elish.lt>
2013-04-16 16:32:27 +0300

    Add coveralls to project. Update readme to show gem version.

--- Gemfile
+++ Gemfile
@@ -3,8 +3,9 @@ source 'https://rubygems.org'
 gem 'rake'
 
 group :test do
+  gem 'coveralls', require: false
   gem 'rspec', '>= 2.13'
-  gem 'simplecov', :require => false
+  gem 'simplecov', require: false
 end
 
 gemspec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
 require 'simplecov'
+require 'coveralls'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  SimpleCov::Formatter::HTMLFormatter
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
 ]
 SimpleCov.start
 

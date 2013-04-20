0a079973521ed11ad94c252fdc6f2d2fd4731796
Justas Palumickas <justas@elish.lt>
2013-04-20 18:51:16 +0300

    Alias for CSstats::Handler.

--- lib/csstats.rb
+++ lib/csstats.rb
@@ -3,6 +3,9 @@ require 'csstats/handler'
 module CSstats
   class << self
 
+    # Alias for CSstats::Handler.new
+    #
+    # Returns CSstats::Handler
     def new(options={})
       CSstats::Handler.new(options)
     end

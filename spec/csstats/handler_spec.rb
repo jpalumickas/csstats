402ad3371593fb65df562ea42e766fc13aa4588f
Justas Palumickas <justas@elish.lt>
2013-09-10 00:17:58 +0300

    Add all stats specs, now we can play hard

--- spec/csstats/handler_spec.rb
+++ spec/csstats/handler_spec.rb
@@ -28,6 +28,78 @@ describe CSstats::Handler do
       expect(@player.rank).to eq 2
     end
 
+    it 'should return correct player teamkill' do
+      expect(@player.teamkill).to eq 7
+    end
+
+    it 'should return correct player damage' do
+      expect(@player.damage).to eq 101543
+    end
+
+    it 'should return correct player deaths' do
+      expect(@player.deaths).to eq 511
+    end
+
+    it 'should return correct player kills' do
+      expect(@player.kills).to eq 759
+    end
+
+    it 'should return correct player shots' do
+      expect(@player.shots).to eq 9421
+    end
+
+    it 'should return correct player hits' do
+      expect(@player.hits).to eq 2885
+    end
+
+    it 'should return correct player headshots' do
+      expect(@player.headshots).to eq 225
+    end
+
+    it 'should return correct player defusions' do
+      expect(@player.defusions).to eq 15
+    end
+
+    it 'should return correct player defused' do
+      expect(@player.defused).to eq 9
+    end
+
+    it 'should return correct player plants' do
+      expect(@player.plants).to eq 33
+    end
+
+    it 'should return correct player explosions' do
+      expect(@player.explosions).to eq 7
+    end
+
+    it 'should return correct player head' do
+      expect(@player.head).to eq 275
+    end
+
+    it 'should return correct player chest' do
+      expect(@player.chest).to eq 407
+    end
+
+    it 'should return correct player stomach' do
+      expect(@player.stomach).to eq 376
+    end
+
+    it 'should return correct player leftarm' do
+      expect(@player.leftarm).to eq 1126
+    end
+
+    it 'should return correct player rightarm' do
+      expect(@player.rightarm).to eq 283
+    end
+
+    it 'should return correct player leftleg' do
+      expect(@player.leftleg).to eq 202
+    end
+
+    it 'should return correct player rightleg' do
+      expect(@player.rightleg).to eq 214
+    end
+
     it 'should return correct player accuracy' do
       expect(@player.acc).to eq 30.62
     end
